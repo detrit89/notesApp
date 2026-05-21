25f3b20bb960b022ba5f977ed2e22ac209d25e74
detrit89 <fogmrt@gmail.com>
2026-05-21 16:22:45 +0700

    dw

--- project/commands.js
+++ project/commands.js
@@ -1,26 +1,42 @@
 import { error } from 'node:console'
 import { deleteNote, searchNote, addNote, checkList, helpCommand} from './handlers.js'
 
-function runCommand(command, argument) {
-    if (command.length <= 0 && argument.length <=0 ) {
-        console.log(error)
+export function runCommand(command, argument) {
+    if (!command) {
+        console.log("Write command")
         return
     }
-}
-
-if (command === "add") {
-    addNote(argument)
-}
-if (command === "list") {
-    checkList()
-}
-if (command === "search") {
-    searchNote(argument)
-}
-if (command === "delete") {
-    deleteNote(argument)
-}
-
-if (command === "help") {
-    helpCommand()
+    if (command === "add") {
+        if (!argument) {
+            console.log("Write note text")
+            return
+        }
+        addNote(argument)
+        return
+    }
+    if (command === "list") {
+        checkList()
+        return
+    }
+    if (command === "search") {
+        if (!argument) {
+            console.log("Write search text")
+            return
+        }
+        searchNote(argument)
+        return
+    }
+    if (command === "delete") {
+        if (!argument) {
+            console.log("Write note id")
+            return
+        }
+        deleteNote(argument)
+        return
+    }
+    if (command === "help") {
+        helpCommand()
+        return
+    }
+    console.log("Unknown command")
 }
